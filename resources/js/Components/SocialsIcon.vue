883d746e3d2b9f9bb9a5ea4866f55ae3230b1e68
martinsr <martinsrucevskis@gmail.com>
2024-06-04 17:25:14 +0300

    final touches

--- resources/js/Components/SocialsIcon.vue
+++ resources/js/Components/SocialsIcon.vue
@@ -2,6 +2,7 @@
 defineProps({
     svg: String,
     color: String,
+    hrefs: String
 
 })
 </script>
@@ -13,7 +14,7 @@ defineProps({
         data-twe-ripple-color="light"
         :style="'background-color:' + color"
         class="mb-2 inline-block rounded-full p-3 m-1 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
-          <span class="[&>svg]:h-4 [&>svg]:w-4">
+          <a :href="hrefs" target="_blank" class="[&>svg]:h-4 [&>svg]:w-4">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -22,7 +23,7 @@ defineProps({
               <path
                   :d="svg" />
             </svg>
-          </span>
+          </a>
     </button>
 </template>
 
